d08aba4b312f04406c1cfd3c952b04aeed731f9f
Antoni <antoni@cypha.de>
2017-04-20 10:26:41 +0200

    From TimlockMultisig to InterPlanetaryArchive

--- createTing.sol
+++ createTing.sol
@@ -1,7 +1,7 @@
 /*
 The MIT License (MIT)
 
-Copyright (c) 2016 Alex van de Sande
+Copyright (c) 2017 Antoni Hauptmann
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of
 this software and associated documentation files (the "Software"), to deal in
@@ -20,35 +20,6 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
-
-
-# Time Locked Multisig
-This is a different twist on the Congress DAO / Multisig. Instead of every
-action requiring the approval of an X number of members, instead any
-transactions can be initiated by a single member, but they all will require a
-minimum amount of delay before they can be executed, which varies according to
-the support that transaction has. The more approvals a proposal has, the sooner
-it can be executed. A member can vote against a transaction, which will mean
-that it will cancel one of the other approved signatures. In a 5 members DAO,
-each vote means the time to wait dimishes by 10x.
-
-This means that if you don't have urgency, one or two signatures might be all
-you need to execute any transaction. But if a single key is compromised, other
-keys can delay that transaction for years, making sure that the main account is
-emptied out way before that.
-
-Transaction delays:
-
-(Support - oppositon)/ total members => Time delay (approximate)
-
-100% approval: 30 min
-90%:           1h30
-80%:           5 hours
-50%:           about a week
-25%:           4 months 
-10%:           2 years
-support=oppos: 5 years or more
--10%         18 years
 
 */
 
@@ -72,77 +43,68 @@ contract owned {
     }
 }
 
-/// @author Alex van de Sande <avsa@ethereum.org>
-contract TimeLockMultisig is owned {
-    Proposal[] public proposals;
-    uint public numProposals;
-    mapping (address => uint) public memberId;
+contract InterPlanetaryArchive is owned {
+    Ting[] public tings;
+    uint public numTings;
+    mapping (address => uint) public isMember;
     Member[] public members;
 
-    event ProposalAdded(uint proposalID, address recipient, uint amount, string description);
-    event Voted(uint proposalID, bool position, address voter, string justification);
-    event ProposalExecuted(uint proposalID, int result, uint deadline);
+    event TingAdded(bytes32 tingID, address creator, uint time, string description);
+    event TingTransferred(bytes32 tingID, address recipient, address sender, bytes32 txID);
     event MembershipChanged(address member, bool isMember);
-    event ChangeOfRules(uint minimumQuorum, uint debatingPeriodInMinutes, int majorityMargin);
 
-    struct Proposal {
-        address recipient;
-        uint amount;
+    struct Ting {
+        bytes32 tingId;
+        address currentOwner;
+        address creator;
         string description;
-        bool executed;
-        int currentResult;
-        bytes32 proposalHash;
-        Vote[] votes;
-        mapping (address => bool) voted;
+        string customData;
+        uint creationTime;
+        Member[] owners;
+        Ting[] tings;
     }
 
     struct Member {
         address member;
-        bool canVote;
+        bool canCreate;
         string name;
         uint memberSince;
     }
 
-    struct Vote {
-        bool inSupport;
-        address voter;
-        string justification;
-    }
 
-    /* modifier that allows only shareholders to vote and create new proposals */
+    /* modifier that allows only shareholders to create new Tings and transfer */
     modifier onlyMembers {
         if (
-            memberId[msg.sender] == 0 ||
-            !members[memberId[msg.sender]].canVote
+            isMember[msg.sender] == 0 ||
+            !members[isMember[msg.sender]].canCreate
         )
             throw;
         _;
     }
 
     /* First time setup */
-    function TimeLockMultisig(address founder, address[] initialMembers) payable {
+    function InterPlanetaryArchive(address founder) payable {
         if (founder != 0) owner = founder;
         // It’s necessary to add an empty first member
         changeMembership(0, false, ''); 
         // and let's add the founder, to save a step later
-        changeMembership(owner, true, 'founder');
-        changeMembers(initialMembers, true);
+        changeMembership(owner, true, 'Founder');
     }
 
     /*make member*/
-    function changeMembership(address targetMember, bool canVote, string memberName) onlyOwner {
+    function changeMembership(address targetMember, bool canCreate, string memberName) onlyOwner {
         uint id;
-        if (memberId[targetMember] == 0) {
-           memberId[targetMember] = members.length;
+        if (isMember[targetMember] == 0) {
+           isMember[targetMember] = members.length;
            id = members.length++;
-           members[id] = Member({member: targetMember, canVote: canVote, memberSince: now, name: memberName});
+           members[id] = Member({member: targetMember, canCreate: canCreate, memberSince: now, name: memberName});
         } else {
-            id = memberId[targetMember];
+            id = isMember[targetMember];
             Member m = members[id];
-            m.canVote = canVote;
+            m.canCreate = canCreate;
         }
 
-        MembershipChanged(targetMember, canVote);
+        MembershipChanged(targetMember, canCreate;
 
     }
 
@@ -152,109 +114,33 @@ contract TimeLockMultisig is owned {
         }
     }
 
-    /* Function to create a new proposal */
-    function newProposal(
-        address beneficiary,
-        uint weiAmount,
-        string jobDescription,
+    /* Function to create a new Ting */
+    function newTing(
+        address creator,
+        string tingDescription,
+        string customData,
         bytes transactionBytecode
     )
         onlyMembers
-        returns (uint proposalID)
+        returns (bytes32 tingID)
     {
-        proposalID = proposals.length++;
-        Proposal p = proposals[proposalID];
-        p.recipient = beneficiary;
-        p.amount = weiAmount;
-        p.description = jobDescription;
-        p.proposalHash = keccak256(beneficiary, weiAmount, transactionBytecode);
-        p.executed = false;
-        ProposalAdded(proposalID, beneficiary, weiAmount, jobDescription);
-        numProposals = proposalID+1;
-        vote(proposalID, true, '');
+        tl = tings.length++;
+        Ting t = tings[tl];
+        //Basic Ting data
+        t.tingId = keccak256(creator, now, transactionBytecode);
+        t.currentOwnern = creator;
+        t.creator = creator;
+        t.description = tingDescription;
+        t.customData = customData;
+        t.creationTime = now;
+        //Array Ting data
+        t.owners[0] = creator;
+        t.tings[0] = '';
+        // Fire Event, raise num counter & register new ting
+        TingAdded(t.tingId, t.creator, t.creationTime, t.description);
+        numTings = tl+1;
 
-        return proposalID;
-    }
-
-
-    /* Function to create a new proposal */
-    function newProposalInEther(
-        address beneficiary,
-        uint etherAmount,
-        string jobDescription,
-        bytes transactionBytecode
-    )
-        onlyMembers
-        returns (uint proposalID)
-    {
-        return newProposal(beneficiary, etherAmount * 1 ether, jobDescription, transactionBytecode);
-    }
-
-    /* function to check if a proposal code matches */
-    function checkProposalCode(
-        uint proposalNumber,
-        address beneficiary,
-        uint etherAmount,
-        bytes transactionBytecode
-    )
-        constant
-        returns (bool codeChecksOut)
-    {
-        Proposal p = proposals[proposalNumber];
-        return p.proposalHash == keccak256(beneficiary, etherAmount, transactionBytecode);
-    }
-
-    function vote(
-        uint proposalNumber,
-        bool supportsProposal,
-        string justificationText
-    )
-        onlyMembers
-        returns (uint voteID)
-    {
-        Proposal p = proposals[proposalNumber];         // Get the proposal
-        if (p.voted[msg.sender] == true) throw;         // If has already voted, cancel
-        p.voted[msg.sender] = true;                     // Set this voter as having voted
-        if (supportsProposal) {                         // If they support the proposal
-            p.currentResult++;                          // Increase score
-        } else {                                        // If they don't
-            p.currentResult--;                          // Decrease the score
-        }
-        // Create a log of this event
-        Voted(proposalNumber,  supportsProposal, msg.sender, justificationText);
-    }
-
-    function proposalDeadline(uint proposalNumber) constant returns(uint deadline) {
-        Proposal p = proposals[proposalNumber];
-        uint factor = 3*10**uint(6 - (5 * p.currentResult / int(members.length - 1)));
-        return now + uint(factor * 1 minutes);
-    }
-
-    function executeProposal(uint proposalNumber, bytes transactionBytecode) returns (int result) {
-        Proposal p = proposals[proposalNumber];
-        /* Check if the proposal can be executed:
-           - Has the voting deadline arrived?
-           - Has it been already executed or is it being executed?
-           - Does the transaction code match the proposal?
-           - Has a minimum quorum?
-        */
-
-        if (
-            now < proposalDeadline(proposalNumber) ||
-            p.currentResult <= 0 ||
-            p.executed ||
-            p.proposalHash != keccak256(p.recipient, p.amount, transactionBytecode)
-        )
-            throw;
-
-
-        p.executed = true;
-        if (!p.recipient.call.value(p.amount)(transactionBytecode)) {
-            throw;
-        }
-
-        // Fire Events
-        ProposalExecuted(proposalNumber, p.currentResult, proposalDeadline(proposalNumber));
+        return t.tingId;
     }
 
     function () payable {}
