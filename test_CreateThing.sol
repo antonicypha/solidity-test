5ea68e975add6f4d8c87c76ffade670c785511c4
antonicypha <antoni@cypha.de>
2017-05-03 20:58:26 +0200

    Update test_CreateThing.sol

--- test_CreateThing.sol
+++ test_CreateThing.sol
@@ -15,17 +15,23 @@ contract owned {
     function transferOwnership(address newOwner) onlyOwner {
         owner = newOwner;
     }
+    
+    /* modifier that allows only shareholders to create new Tings and transfer */
+    modifier onlyMembers {
+        if (
+            isMember[msg.sender] == 0 ||
+            !members[isMember[msg.sender]].canCreate
+        )
+            throw;
+        _;
+    }
 }
 
-contract InterPlanetaryArchive is owned {
+contract CreateThing is owned {
     Ting[] public tings;
     uint public numTings;
-    mapping (address => uint) public isMember;
-    Member[] public members;
 
     event TingAdded(bytes32 tingID, address creator, uint time, string description);
-    event TingTransferred(bytes32 tingID, address recipient, address sender, bytes32 txID);
-    event MembershipChanged(address member, bool isMember);
 
     struct Ting {
         bytes32 tingId;
@@ -38,54 +44,8 @@ contract InterPlanetaryArchive is owned {
         Ting[] tings;
     }
 
-    struct Member {
-        address member;
-        bool canCreate;
-        string name;
-        uint memberSince;
-    }
-
-
-    /* modifier that allows only shareholders to create new Tings and transfer */
-    modifier onlyMembers {
-        if (
-            isMember[msg.sender] == 0 ||
-            !members[isMember[msg.sender]].canCreate
-        )
-            throw;
-        _;
-    }
-
-    /* First time setup */
-    function InterPlanetaryArchive() payable {
+    function CreateThing() payable {
         
-        // It’s necessary to add an empty first member #cypha: not sure why, I just init the 1st entry
-        changeMembership(msg.sender, false, 'Init'); 
-        // and let's add the founder, to save a step later
-        changeMembership(owner, true, 'Founder');
-    }
-
-    /*make member*/
-    function changeMembership(address targetMember, bool canCreate, string memberName) onlyOwner {
-        uint id;
-        if (isMember[targetMember] == 0) {
-           isMember[targetMember] = members.length;
-           id = members.length++;
-           members[id] = Member({member: targetMember, canCreate: canCreate, memberSince: now, name: memberName});
-        } else {
-            id = isMember[targetMember];
-            Member m = members[id];
-            m.canCreate = canCreate;
-        }
-
-        MembershipChanged(targetMember, canCreate);
-
-    }
-
-    function changeMembers(address[] newMembers, bool canVote) {
-        for (uint i = 0; i < newMembers.length; i++) {
-            changeMembership(newMembers[i], canVote, '');
-        }
     }
 
     /* Function to create a new Ting */
@@ -93,9 +53,7 @@ contract InterPlanetaryArchive is owned {
         address creator,
         string tingDescription,
         string customData,
-        bytes transactionBytecode
     )
-        onlyMembers
         returns (bytes32 tingID)
     {
         tl = tings.length++;
@@ -109,7 +67,6 @@ contract InterPlanetaryArchive is owned {
         t.creationTime = now;
         //Array Ting data
         t.owners[0] = creator;
-        //t.tings[0] = '';
         // Fire Event, raise num counter & register new ting
         TingAdded(t.tingId, t.creator, t.creationTime, t.description);
         numTings = tl+1;
